10c6638375d1a79f1db76316867375157469493a
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-03-03 14:29:08 -0300

    feat: Error handling

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/health', (request, reponse) => {
-  return reponse.json({ message: 'Hello Dev! I am Alive! ' });
+routes.get('/health', (request, response) => {
+  return response.json({ message: 'Hello Dev! I am Alive! ' });
 });
 
 export default routes;
