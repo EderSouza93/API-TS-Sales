8bd58d3d70479a791626824f0c314bac39528d3a
EderSouza93 <ederaldo.souza93@gmail.com>
2025-03-12 14:37:36 -0300

    feat: route avatar get

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,9 @@
+import uploadConfig from '@config/upload';
 import productsRouter from '@modules/products/routes/ProductRoutes';
 import avatarRouter from '@modules/users/routes/AvatarRoutes';
 import sessionsRouter from '@modules/users/routes/SessionRoutes';
 import usersRouter from '@modules/users/routes/UserRoutes';
-import { Router, Request, Response } from 'express';
+import express, { Router, Request, Response } from 'express';
 
 const routes = Router();
 
@@ -13,5 +14,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/avatar', avatarRouter);
+routes.use('/files', express.static(uploadConfig.directory));
 
 export default routes;
