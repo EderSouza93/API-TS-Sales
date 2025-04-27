74aa528f153c6fc4d44eac5a1abf31e80613f7c0
EderSouza93 <ederaldo.souza93@gmail.com>
2025-04-27 15:12:36 -0300

    First test create customer

--- src/modules/customers/services/CreateCustumersService.spec.ts
+++ src/modules/customers/services/CreateCustumersService.spec.ts
@@ -1,6 +1,19 @@
+import FakeCustomerRepositories from '../domain/repositories/fakes/FakeCustomerRepositories';
+import CreateCustomerService from './CreateCustomersService';
+
 describe('CreateCustomerService', () => {
-  it('should be able to create a new customer', () => {
-    expect(1 + 1).toBe(2);
+  it('should be able to create a new customer', async () => {
+    const fakeCustomerRepository = new FakeCustomerRepositories();
+    const createCustomer = new CreateCustomerService(fakeCustomerRepository);
+
+    const customer = await createCustomer.execute({
+      name: 'John Doe',
+      email: 'john@gmail.com',
+    });
+
+    expect(customer).toHaveProperty('id');
+    expect(customer.name).toBe('John Doe');
+    expect(customer.email).toBe('john@gmail.com');
   });
   it('should not be able to create a new customer with email that is already in use', () => {
     expect(1 + 1).toBe(2);
