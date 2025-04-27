9a7de1e968a8a288e969ac9b0707c7d5683ccc06
EderSouza93 <ederaldo.souza93@gmail.com>
2025-04-27 14:16:25 -0300

    create test

---
+++ src/modules/customers/services/CreateCustumersService.spec.ts
@@ -0,0 +1,8 @@
+describe('CreateCustomerService', () => {
+  it('should be able to create a new customer', () => {
+    expect(1 + 1).toBe(2);
+  });
+  it('should not be able to create a new customer with email that is already in use', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
